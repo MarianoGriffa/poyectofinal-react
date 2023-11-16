1e73a61a43b2e415b7be02807752674e0c95c5c1
Mariano Griffa <marianogriffa@hotmail.com>
2023-11-16 18:12:08 -0300

    Retoques en footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,11 +8,11 @@ export const Footer = () => {
               <Content style={{ textAlign: 'center'}}>
                 <p>  
                   <strong>KiwiStore</strong> por {''}  
-                  <a href="#">Griffa Mariano</a>© Todos los derechos reservados - 2023
-                  .
-                </p>    
-              </Content> 
-            </Container>
+                  <a href="mailto:someone@example.com">Griffa Mariano</a> 
+                   © Todos los derechos reservados - 2023.  
+                </p>          
+              </Content>    
+            </Container>   
           </footer>   
   );  
 
